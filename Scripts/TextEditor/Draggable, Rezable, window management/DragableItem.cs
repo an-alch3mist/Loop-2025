a822a8954eda516ab99a0ab56e4f889155b413be
an-alch3mist <kaeya000@gmail.com>
2025-08-02 03:59:56 +0600

    7

--- Scripts/TextEditor/Draggable, Rezable, window management/DragableItem.cs
+++ Scripts/TextEditor/Draggable, Rezable, window management/DragableItem.cs
@@ -1,257 +1,260 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
+
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 using SPACE_UTIL;
-using System;
 
-// works only on left mouse click event
-public class ADragableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+namespace GptDeepResearch
 {
-	[SerializeField] RectTransform entireWindowRect;
-
-	Vector2 delta;
-	Transform entireWindowParentBeforeDrag;
-	public void OnBeginDrag(PointerEventData eventData)
+	// works only on left mouse click event
+	public class ADragableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 	{
-		Debug.Log("OnBeginDrag");
-		if (this.entireWindowRect == null)
-			this.entireWindowRect = this.gameObject.GC<RectTransform>();
+		[SerializeField] RectTransform entireWindowRect;
 
-		this.entireWindowParentBeforeDrag = this.entireWindowRect.parent;
-		this.entireWindowRect.SetParent(this.entireWindowRect.root);
-		this.entireWindowRect.SetAsLastSibling();
-
-		this.delta = entireWindowRect.anchoredPosition - INPUT.UI.pos;
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-		// restriction within bounds >>
-
-		// << restriction within bounds
-		entireWindowRect.anchoredPosition = targetPos;
-
-		//throw new System.NotImplementedException();
-	}
-
-	public void OnDrag(PointerEventData eventData)
-	{
-		Debug.Log("OnDrag");
-		// this.delta = WindowRect.anchoredPosition - INPUT.UI.pos;
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-		// restriction within bounds >>
-
-		// << restriction within bounds
-		entireWindowRect.anchoredPosition = targetPos;
-		//throw new System.NotImplementedException();
-	}
-
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		Debug.Log("OnEndDrag");
-		// this.delta = WindowRect.anchoredPosition - INPUT.UI.pos;
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-		// restriction within bounds >>
-
-		// << restriction within bounds
-		entireWindowRect.anchoredPosition = targetPos;
-
-		this.entireWindowRect.parent = this.entireWindowParentBeforeDrag;
-		//throw new System.NotImplementedException();
-	}
-
-
-}
-
-
-
-// Works with custom mouse buttons (Left, Right, Middle)
-public class DragableItem : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
-{
-	[Header("Mouse Button Settings")]
-	public bool useMiddleMouseButton = true;
-	public bool useLeftMouseButton = true;
-	public bool useRightMouseButton = true;
-
-	[SerializeField] RectTransform entireWindowRect;
-
-	private Vector2 delta;
-	private Transform entireWindowParentBeforeDrag;
-	private bool isDragging = false;
-	private bool isMouseOver = false;
-	private int dragButton = -1; // Which button started the drag (0=Left, 1=Right, 2=Middle)
-
-	void Update()
-	{
-		if (isDragging)
+		Vector2 delta;
+		Transform entireWindowParentBeforeDrag;
+		public void OnBeginDrag(PointerEventData eventData)
 		{
-			// Continue dragging while button is held
-			bool buttonStillDown = false;
+			Debug.Log("OnBeginDrag");
+			if (this.entireWindowRect == null)
+				this.entireWindowRect = this.gameObject.GC<RectTransform>();
 
-			if (dragButton == 0 && Input.GetMouseButton(0)) buttonStillDown = true;
-			else if (dragButton == 1 && Input.GetMouseButton(1)) buttonStillDown = true;
-			else if (dragButton == 2 && Input.GetMouseButton(2)) buttonStillDown = true;
+			this.entireWindowParentBeforeDrag = this.entireWindowRect.parent;
+			this.entireWindowRect.SetParent(this.entireWindowRect.root);
+			this.entireWindowRect.SetAsLastSibling();
 
-			if (buttonStillDown)
+			this.delta = entireWindowRect.anchoredPosition - INPUT.UI.pos;
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+			// restriction within bounds >>
+
+			// << restriction within bounds
+			entireWindowRect.anchoredPosition = targetPos;
+
+			//throw new System.NotImplementedException();
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			Debug.Log("OnDrag");
+			// this.delta = WindowRect.anchoredPosition - INPUT.UI.pos;
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+			// restriction within bounds >>
+
+			// << restriction within bounds
+			entireWindowRect.anchoredPosition = targetPos;
+			//throw new System.NotImplementedException();
+		}
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			Debug.Log("OnEndDrag");
+			// this.delta = WindowRect.anchoredPosition - INPUT.UI.pos;
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+			// restriction within bounds >>
+
+			// << restriction within bounds
+			entireWindowRect.anchoredPosition = targetPos;
+
+			this.entireWindowRect.parent = this.entireWindowParentBeforeDrag;
+			//throw new System.NotImplementedException();
+		}
+
+
+	}
+
+	// detects well compared to drag handler
+	// Works with custom mouse buttons (Left, Right, Middle)
+	public class DragableItem : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+	{
+		[Header("Mouse Button Settings")]
+		public bool useMiddleMouseButton = true;
+		public bool useLeftMouseButton = true;
+		public bool useRightMouseButton = true;
+
+		[SerializeField] RectTransform entireWindowRect;
+
+		private Vector2 delta;
+		private Transform entireWindowParentBeforeDrag;
+		private bool isDragging = false;
+		private bool isMouseOver = false;
+		private int dragButton = -1; // Which button started the drag (0=Left, 1=Right, 2=Middle)
+
+		void Update()
+		{
+			if (isDragging)
 			{
-				OnDrag();
+				// Continue dragging while button is held
+				bool buttonStillDown = false;
+
+				if (dragButton == 0 && Input.GetMouseButton(0)) buttonStillDown = true;
+				else if (dragButton == 1 && Input.GetMouseButton(1)) buttonStillDown = true;
+				else if (dragButton == 2 && Input.GetMouseButton(2)) buttonStillDown = true;
+
+				if (buttonStillDown)
+				{
+					OnDrag();
+				}
+				else
+				{
+					OnEndDrag();
+				}
 			}
-			else
+			else if (isMouseOver)
 			{
-				OnEndDrag();
+				// Check for drag start with custom mouse buttons
+				if (useMiddleMouseButton && Input.GetMouseButtonDown(2))
+				{
+					OnBeginDrag(2);
+				}
+				else if (useRightMouseButton && Input.GetMouseButtonDown(1))
+				{
+					OnBeginDrag(1);
+				}
+				else if (useLeftMouseButton && Input.GetMouseButtonDown(0))
+				{
+					OnBeginDrag(0);
+				}
 			}
 		}
-		else if (isMouseOver)
+
+		public void OnPointerDown(PointerEventData eventData)
 		{
-			// Check for drag start with custom mouse buttons
-			if (useMiddleMouseButton && Input.GetMouseButtonDown(2))
-			{
-				OnBeginDrag(2);
-			}
-			else if (useRightMouseButton && Input.GetMouseButtonDown(1))
-			{
-				OnBeginDrag(1);
-			}
-			else if (useLeftMouseButton && Input.GetMouseButtonDown(0))
+			// Handle left mouse button through EventSystem if enabled
+			if (useLeftMouseButton && eventData.button == PointerEventData.InputButton.Left)
 			{
 				OnBeginDrag(0);
 			}
 		}
-	}
 
-	public void OnPointerDown(PointerEventData eventData)
-	{
-		// Handle left mouse button through EventSystem if enabled
-		if (useLeftMouseButton && eventData.button == PointerEventData.InputButton.Left)
+		public void OnPointerUp(PointerEventData eventData)
 		{
-			OnBeginDrag(0);
-		}
-	}
-
-	public void OnPointerUp(PointerEventData eventData)
-	{
-		// Handle mouse up through EventSystem
-		if (isDragging &&
-			((eventData.button == PointerEventData.InputButton.Left && dragButton == 0) ||
-			 (eventData.button == PointerEventData.InputButton.Right && dragButton == 1) ||
-			 (eventData.button == PointerEventData.InputButton.Middle && dragButton == 2)))
-		{
-			OnEndDrag();
-		}
-	}
-
-	public void OnPointerEnter(PointerEventData eventData)
-	{
-		isMouseOver = true;
-	}
-
-	public void OnPointerExit(PointerEventData eventData)
-	{
-		isMouseOver = false;
-	}
-
-	// Modified version of your original OnBeginDrag
-	public void OnBeginDrag(int buttonIndex)
-	{
-		if (isDragging) return;
-
-		Debug.Log($"OnBeginDrag with button {buttonIndex} (0=Left, 1=Right, 2=Middle)");
-		isDragging = true;
-		dragButton = buttonIndex;
-
-		if (this.entireWindowRect == null)
-		{
-			this.entireWindowRect = this.gameObject.GC<RectTransform>();
+			// Handle mouse up through EventSystem
+			if (isDragging &&
+				((eventData.button == PointerEventData.InputButton.Left && dragButton == 0) ||
+				 (eventData.button == PointerEventData.InputButton.Right && dragButton == 1) ||
+				 (eventData.button == PointerEventData.InputButton.Middle && dragButton == 2)))
+			{
+				OnEndDrag();
+			}
 		}
 
-		this.entireWindowParentBeforeDrag = this.entireWindowRect.parent;
-		this.entireWindowRect.SetParent(this.entireWindowRect.root);
-		this.entireWindowRect.SetAsLastSibling();
-
-		this.delta = entireWindowRect.anchoredPosition - INPUT.UI.pos;
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-
-		// restriction within bounds >>
-		// << restriction within bounds
-
-		entireWindowRect.anchoredPosition = targetPos;
-	}
-
-	// Modified version of your original OnDrag
-	public void OnDrag()
-	{
-		if (!isDragging) return;
-
-		Debug.Log("OnDrag");
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-
-		// restriction within bounds >>
-		// << restriction within bounds
-
-		entireWindowRect.anchoredPosition = targetPos;
-	}
-
-	// Modified version of your original OnEndDrag
-	public void OnEndDrag()
-	{
-		if (!isDragging) return;
-
-		Debug.Log("OnEndDrag");
-		Vector2 targetPos = INPUT.UI.pos + this.delta;
-
-		// restriction within bounds >>
-		// << restriction within bounds
-
-		entireWindowRect.anchoredPosition = targetPos;
-		this.entireWindowRect.parent = this.entireWindowParentBeforeDrag;
-
-		isDragging = false;
-		dragButton = -1;
-	}
-
-	// Legacy method overloads for compatibility (in case other code calls these)
-	public void OnBeginDrag(PointerEventData eventData)
-	{
-		if (useLeftMouseButton && eventData.button == PointerEventData.InputButton.Left)
+		public void OnPointerEnter(PointerEventData eventData)
 		{
-			OnBeginDrag(0);
+			isMouseOver = true;
 		}
-	}
 
-	public void OnDrag(PointerEventData eventData)
-	{
-		// This will be handled by the Update method
-		// keeping this method for interface compatibility
-	}
-
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		if (isDragging && eventData.button == PointerEventData.InputButton.Left && dragButton == 0)
+		public void OnPointerExit(PointerEventData eventData)
 		{
-			OnEndDrag();
+			isMouseOver = false;
 		}
-	}
 
-	// Public utility methods
-	public bool IsDragging()
-	{
-		return isDragging;
-	}
+		// Modified version of your original OnBeginDrag
+		public void OnBeginDrag(int buttonIndex)
+		{
+			if (isDragging) return;
 
-	public int GetDragButton()
-	{
-		return dragButton;
-	}
+			Debug.Log($"OnBeginDrag with button {buttonIndex} (0=Left, 1=Right, 2=Middle)");
+			isDragging = true;
+			dragButton = buttonIndex;
 
-	public void SetDragButtons(bool middle, bool left, bool right)
-	{
-		useMiddleMouseButton = middle;
-		useLeftMouseButton = left;
-		useRightMouseButton = right;
-	}
+			if (this.entireWindowRect == null)
+			{
+				this.entireWindowRect = this.gameObject.GC<RectTransform>();
+			}
 
-	// Method to manually set the window rect if needed
-	public void SetEntireWindowRect(RectTransform windowRect)
-	{
-		this.entireWindowRect = windowRect;
+			this.entireWindowParentBeforeDrag = this.entireWindowRect.parent;
+			this.entireWindowRect.SetParent(this.entireWindowRect.root);
+			this.entireWindowRect.SetAsLastSibling();
+
+			this.delta = entireWindowRect.anchoredPosition - INPUT.UI.pos;
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+
+			// restriction within bounds >>
+			// << restriction within bounds
+
+			entireWindowRect.anchoredPosition = targetPos;
+		}
+
+		// Modified version of your original OnDrag
+		public void OnDrag()
+		{
+			if (!isDragging) return;
+
+			Debug.Log("OnDrag");
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+
+			// restriction within bounds >>
+			// << restriction within bounds
+
+			entireWindowRect.anchoredPosition = targetPos;
+		}
+
+		// Modified version of your original OnEndDrag
+		public void OnEndDrag()
+		{
+			if (!isDragging) return;
+
+			Debug.Log("OnEndDrag");
+			Vector2 targetPos = INPUT.UI.pos + this.delta;
+
+			// restriction within bounds >>
+			// << restriction within bounds
+
+			entireWindowRect.anchoredPosition = targetPos;
+			this.entireWindowRect.parent = this.entireWindowParentBeforeDrag;
+
+			isDragging = false;
+			dragButton = -1;
+		}
+
+		// Legacy method overloads for compatibility (in case other code calls these)
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			if (useLeftMouseButton && eventData.button == PointerEventData.InputButton.Left)
+			{
+				OnBeginDrag(0);
+			}
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			// This will be handled by the Update method
+			// keeping this method for interface compatibility
+		}
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			if (isDragging && eventData.button == PointerEventData.InputButton.Left && dragButton == 0)
+			{
+				OnEndDrag();
+			}
+		}
+
+		// Public utility methods
+		public bool IsDragging()
+		{
+			return isDragging;
+		}
+
+		public int GetDragButton()
+		{
+			return dragButton;
+		}
+
+		public void SetDragButtons(bool middle, bool left, bool right)
+		{
+			useMiddleMouseButton = middle;
+			useLeftMouseButton = left;
+			useRightMouseButton = right;
+		}
+
+		// Method to manually set the window rect if needed
+		public void SetEntireWindowRect(RectTransform windowRect)
+		{
+			this.entireWindowRect = windowRect;
+		}
 	}
 }
